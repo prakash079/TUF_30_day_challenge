836836ea04aca681e267dff73bc537bbf411f5ad
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-09-13 01:45:36 +0530

    Update Day_1-Question01.py
    added the optimized solution

--- Day_1-Question01.py
+++ Day_1-Question01.py
@@ -14,3 +14,26 @@ so after sorting this :1,2,3,4,5,5 so now if we compare ith element with (i+1)th
 
 #TC = O(nlogn)
 #space = O(1)
+
+
+#here is the updated solution for the same
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        # Find the "entrance" to the cycle.
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return fast
+    
+/* here i have used the principle of FLOYDs ALGORITHM to solve this problem */
+#TC = o(n)
+#space = o(1)
