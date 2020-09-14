5fbb71768292f0b8ea3e6049f922d26e96ca875f
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-09-15 02:20:14 +0530

    Create Day_1-question3.py

---
+++ Day_1-question3.py
@@ -0,0 +1,11 @@
+#question-link=https://practice.geeksforgeeks.org/problems/find-missing-and-repeating/
+
+--------------------------------------------------------------------------------------
+#solution
+class Solution:
+    def findTwoElement( self,l, n):
+        ans=[0]*2
+        k=list(set(l))
+        ans[1]=((n*(n+1))//2)-sum(k)
+        ans[0]=sum(l)-sum(k)
+        return(ans)
