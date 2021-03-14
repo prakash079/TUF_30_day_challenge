5bc35328cbf9c4a5a288c34db0f6132a890fc6fd
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2021-03-14 15:42:28 +0530

    Create Day_1-Question003.cpp
    Optimized solution in cpp

---
+++ Day_1-Question003.cpp
@@ -0,0 +1,74 @@
+//question link==https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/
+
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int i=n-1,j=0;
+            while(i>=0 && j<m)
+            {
+                if(arr1[i]>arr2[j])
+                {
+                    long long t=arr1[i];
+                    arr1[i]=arr2[j];
+                    arr2[j]=t;
+                    //sort(arr2,arr2+m);
+                    //i++;
+                    //i--;
+                }
+                i--;
+                j++;
+                
+            }
+            sort(arr1,arr1+n);
+            sort(arr2,arr2+m);
+            // code here 
+        } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
