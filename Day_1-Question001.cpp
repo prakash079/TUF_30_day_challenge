827b88f5314192626c589d94fb1521d5111cff20
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2021-03-14 13:14:15 +0530

    Create Day_1-Question001.cpp
    This solution works with o(N) time and o(1) extra space

---
+++ Day_1-Question001.cpp
@@ -0,0 +1,33 @@
+//this is more optimized solution
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        //for(int i=0;i<n;i++)
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                int t=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=t;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                int t=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=t;
+                high--;
+            }
+        }
+        
+    }
+};
